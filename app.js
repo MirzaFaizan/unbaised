15a2fb39fe3c935aa3fa3f4abeb2fbc4c3d2a9ed
Omer Farooq <comingbacktolyf@gmail.com>
2019-06-26 14:52:50 +0500

    Added Arrow Syntax

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.user = req.session.user;
   res.locals.isauth = req.isAuthenticated();
   next();
