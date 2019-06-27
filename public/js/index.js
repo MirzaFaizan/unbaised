512bdd5d3048bc52c4266da3ffc967b1e16921ec
Omer Farooq <comingbacktolyf@gmail.com>
2019-06-27 10:44:51 +0500

    Added Welcome Message

--- public/js/index.js
+++ public/js/index.js
@@ -1,51 +1,60 @@
-$("document").ready(function () {
-
-    // function that converts all time dates on a page to relative time
-    $("time").each(function (index) {
-        $(this).text(moment($(this).attr('title')).fromNow())
+$('document').ready(function() {
+  if (localStorage.getItem('firstTime') =s= null) {
+    swal({
+      title: 'For the People By the People!',
+      text:
+        'User post links to Articles from your common news source.\n \n Other vote whether it swings Left Right or Neutral',
+      icon: 'success',
+      button: 'Go To Feed!'
     });
+    localStorage.setItem('firstTime', 'done');
+  }
 
-    // event handler that replaces broken thumbnails with a default one.
-    $(".thumbnail").on('error', function () {
-        $(this).attr('src', '../images/image.png');
-    });
+  // function that converts all time dates on a page to relative time
+  $('time').each(function(index) {
+    $(this).text(moment($(this).attr('title')).fromNow());
+  });
 
-    // event handler that checks if the user is authenticated when they click an auth-only action.
-    $(".auth-req").click(function (e) {
-        if ($("#auth").text() == "false") {
-            alert("You must be logged in to do that.");
-            e.stopImmediatePropagation();
-            return false;
-        }
-    });
+  // event handler that replaces broken thumbnails with a default one.
+  $('.thumbnail').on('error', function() {
+    $(this).attr('src', '../images/image.png');
+  });
 
-    // event handler for when user subscribes to subreddit
-    $("#subscribe").click(function () {
-        let sub = $("#subreddit-name").text();
-        let that = $(this)
+  // event handler that checks if the user is authenticated when they click an auth-only action.
+  $('.auth-req').click(function(e) {
+    if ($('#auth').text() == 'false') {
+      alert('You must be logged in to do that.');
+      e.stopImmediatePropagation();
+      return false;
+    }
+  });
 
-        // if current text is subscribe, update subscription in database
-        if ($(this).text() == "Subscribe") {
-            $.ajax({
-                type: "put",
-                url: `/subscribe/${sub}`
-            }).done(function (res) {
-                that.text('Unsubscribe')
-                alert(`You are now subscribed to ${sub}`)
-            })
+  // event handler for when user subscribes to subreddit
+  $('#subscribe').click(function() {
+    let sub = $('#subreddit-name').text();
+    let that = $(this);
 
-            // if current text is unsubscribe, update subscription in database
-        } else if ($(this).text() == "Unsubscribe") {
-            alert('unsubscribe text')
-            $.ajax({
-                type: "put",
-                url: `/unsubscribe/${sub}`,
-            }).done(function (res) {
-                that.text('Subscribe')
-                alert(`You are now unsbscribed from ${sub}`)
-            })
-        }
-        return false;
-    })
+    // if current text is subscribe, update subscription in database
+    if ($(this).text() == 'Subscribe') {
+      $.ajax({
+        type: 'put',
+        url: `/subscribe/${sub}`
+      }).done(function(res) {
+        that.text('Unsubscribe');
+        alert(`You are now subscribed to ${sub}`);
+      });
 
+      // if current text is unsubscribe, update subscription in database
+    } else if ($(this).text() == 'Unsubscribe') {
+      alert('unsubscribe text');
+      $.ajax({
+        type: 'put',
+        url: `/unsubscribe/${sub}`
+      }).done(function(res) {
+        that.text('Subscribe');
+        alert(`You are now unsbscribed from ${sub}`);
+      });
+    }
+    return false;
+  });
 });
