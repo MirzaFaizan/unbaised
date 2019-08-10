b818556d3258b9ba48692eacc8d41c3fde9be28e
Omer Farooq <comingbacktolyf@gmail.com>
2019-08-10 13:19:05 +0500

    Added Welcome Video

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,11 +1,17 @@
 $('document').ready(function() {
-  if (localStorage.getItem('firstTime') == null) {
-    swal({
-      title: 'For the People By the People!',
-      text:
-        'User post links to Articles from your common news sources.\n \n Other vote whether it swings Left Right or Neutral',
-      icon: 'success',
-      button: 'View Articles'
+  if (localStorage.getItem('firstTime')) {
+    // swal({
+    //   title: 'For the People By the People!',
+    //   text:
+    //     'User post links to Articles from your common news sources.\n \n Other vote whether it swings Left Right or Neutral',
+    //   icon: 'success',
+    //   button: 'View Articles'
+    // });
+    Swal.fire({
+      title: '<strong class="display-4">Unbiased</strong>',
+      html: ` <video src="/js/welcome.mp4" onloadedmetadata="this.muted = true" playsinline autoplay muted controls></video>`,
+      showCloseButton: true,
+      showCancelButton: true
     });
     localStorage.setItem('firstTime', 'done');
   }
